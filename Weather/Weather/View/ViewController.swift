3d53621a64d753eae9166263f4b71f339cf95242
FurkanMDemiray <furkanmdemiray@gmail.com>
2024-05-11 15:07:17 +0300

    Added weather model , weather service, main UI Design

---
+++ Weather/Weather/View/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Melik Demiray on 11.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var weatherViewModel = WeatherViewModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
